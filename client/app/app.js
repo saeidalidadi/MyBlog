7f496a1e319e5fcc37f2fefe664ad5c2ce7af8a4
Saeid Alidadi <alidadisaeid@gmail.com>
2016-12-10 22:07:59 +0330

    for post pagination and image

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,8 @@
 angular.module('myblogApp', ['myblogApp.auth', 'myblogApp.admin', 'myblogApp.constants',
     'ngCookies', 'ngResource', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'validation.match'
   ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, uibPaginationConfig) {
     $urlRouterProvider.otherwise('/');
-
+    uibPaginationConfig.itemsPerPage = 2;
     $locationProvider.html5Mode(true);
   });
