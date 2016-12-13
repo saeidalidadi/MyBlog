0ea43959cd8a9c0044d42e46eda07c530d256c42
Saeid Alidadi <alidadisaeid@gmail.com>
2016-12-13 16:22:00 +0330

    Revert "post service scaffolding"
    This reverts commit 3019bb6939fd73e8daa61f931ae7ed343abb6adc.

--- client/app/post/post/post.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-function postService() {
-	// AngularJS will instantiate a singleton by calling "new" on this function
-}
-
-angular.module('myblogApp')
-  .service('post', postService);
