3019bb6939fd73e8daa61f931ae7ed343abb6adc
Saeid Alidadi <alidadisaeid@gmail.com>
2016-12-13 13:43:02 +0330

    post service scaffolding

---
+++ client/app/post/post/post.service.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function postService() {
+	// AngularJS will instantiate a singleton by calling "new" on this function
+}
+
+angular.module('myblogApp')
+  .service('post', postService);
